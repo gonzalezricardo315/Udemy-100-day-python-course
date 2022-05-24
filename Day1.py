3541d10118e5bf5f7cd6e363d38b79b4865e020c
gonzalezricardo315 <Gonzalezricardo315@yahoo.com>
2022-05-23 22:21:22 -0600

    What was done on Day one, all stuff I knew already.

---
+++ Day1.py
@@ -0,0 +1,19 @@
+# practicing getting input with if else statements
+
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm?"))
+#
+# if height > 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry you cant ride the rollercoaster")
+
+
+# 3.1 Write a program that works out whether if a given number is an odd or even number.
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
